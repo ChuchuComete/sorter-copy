a0db76a3a7c5b9b0aa8a99a28d486871cb90adc8
ChuchuComete <160599605+ChuchuComete@users.noreply.github.com>
2024-11-11 13:02:25 +0100

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,7 +1,7 @@
 
 export const songs = [
 	{
-    name: "One Piece - We Are! by Hiroshi Kitadani",
+    name: "One Piece OP1 - We Are! by Hiroshi Kitadani",
     videoId : "https://naedist.animemusicquiz.com/c7pqug.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -11,7 +11,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Believe by Folder5",
+    name: "One Piece OP2 - Believe by Folder5",
     videoId : "https://naedist.animemusicquiz.com/im1a6p.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -21,7 +21,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Hikari e by The Babystars",
+    name: "One Piece OP3 - Hikari e by The Babystars",
     videoId : "https://naedist.animemusicquiz.com/tlds4l.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -31,7 +31,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - BON VOYAGE! by BON-BON BLANCO",
+    name: "One Piece OP4 - BON VOYAGE! by BON-BON BLANCO",
     videoId : "https://naedist.animemusicquiz.com/m2wa5w.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -41,7 +41,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Kokoro no Chizu by BOYSTYLE",
+    name: "One Piece OP5 - Kokoro no Chizu by BOYSTYLE",
     videoId : "https://naedist.animemusicquiz.com/a0g48o.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -51,7 +51,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - BRAND NEW WORLD by D-51",
+    name: "One Piece OP6 - BRAND NEW WORLD by D-51",
     videoId : "https://naedist.animemusicquiz.com/aemf9t.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -61,7 +61,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - We Are!~7-nin no Mugiwara Kaizoku-danpen by 7-nin no Mugiwara Kaizoku-dan",
+    name: "One Piece OP7 - We Are!~7-nin no Mugiwara Kaizoku-danpen by 7-nin no Mugiwara Kaizoku-dan",
     videoId : "https://naedist.animemusicquiz.com/zjypxh.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -71,7 +71,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Crazy Rainbow by Tackey & Tsubasa",
+    name: "One Piece OP8 - Crazy Rainbow by Tackey & Tsubasa",
     videoId : "https://naedist.animemusicquiz.com/9rrce3.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -81,7 +81,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Jungle P by 5050",
+    name: "One Piece OP9 - Jungle P by 5050",
     videoId : "https://naedist.animemusicquiz.com/dh2ojv.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -91,7 +91,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - We Are!~Animation One Piece 10 Shuunen Ver.~ by Tohoshinki",
+    name: "One Piece OP10 - We Are!~Animation One Piece 10 Shuunen Ver.~ by Tohoshinki",
     videoId : "https://naedist.animemusicquiz.com/bwx8pb.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -101,7 +101,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Share The World by Tohoshinki",
+    name: "One Piece OP11 - Share The World by Tohoshinki",
     videoId : "https://naedist.animemusicquiz.com/zdxhyr.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -111,7 +111,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Kaze o Sagashite by Mari Yaguchi to Straw Hat",
+    name: "One Piece OP12 - Kaze o Sagashite by Mari Yaguchi to Straw Hat",
     videoId : "https://naedist.animemusicquiz.com/vw23ce.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -121,7 +121,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - One day by THE ROOTLESS",
+    name: "One Piece OP13 - One day by THE ROOTLESS",
     videoId : "https://naedist.animemusicquiz.com/xadig3.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -131,7 +131,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Fight Together by Namie Amuro",
+    name: "One Piece OP14 - Fight Together by Namie Amuro",
     videoId : "https://naedist.animemusicquiz.com/qqnmz1.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -141,7 +141,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - We Go! by Hiroshi Kitadani",
+    name: "One Piece OP15 - We Go! by Hiroshi Kitadani",
     videoId : "https://naedist.animemusicquiz.com/io0s7q.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -151,7 +151,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - HANDS UP! by Kota Shinzato",
+    name: "One Piece OP16 - HANDS UP! by Kota Shinzato",
     videoId : "https://naedist.animemusicquiz.com/bjm0na.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -161,7 +161,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Wake up! by AAA",
+    name: "One Piece OP17 - Wake up! by AAA",
     videoId : "https://naedist.animemusicquiz.com/425x16.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -171,7 +171,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Hard Knock Days by GENERATIONS from EXILE TRIBE",
+    name: "One Piece OP18 - Hard Knock Days by GENERATIONS from EXILE TRIBE",
     videoId : "https://naedist.animemusicquiz.com/7ghiql.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -181,7 +181,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - We Can! by Kishidan to Hiroshi Kitadani",
+    name: "One Piece OP19 - We Can! by Kishidan to Hiroshi Kitadani",
     videoId : "https://naedist.animemusicquiz.com/acof2e.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -191,7 +191,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Hope by Namie Amuro",
+    name: "One Piece OP20 - Hope by Namie Amuro",
     videoId : "https://naedist.animemusicquiz.com/8z0xet.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -201,7 +201,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Super Powers by V6",
+    name: "One Piece OP21 - Super Powers by V6",
     videoId : "https://naedist.animemusicquiz.com/7f33tu.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -211,7 +211,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - OVER THE TOP by Hiroshi Kitadani",
+    name: "One Piece OP22 - OVER THE TOP by Hiroshi Kitadani",
     videoId : "https://naedist.animemusicquiz.com/apxeiw.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -221,7 +221,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - DREAMIN' ON by Da-iCE",
+    name: "One Piece OP23 - DREAMIN' ON by Da-iCE",
     videoId : "https://naedist.animemusicquiz.com/hnfwlu.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -231,7 +231,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - PAINT by I Don't Like Mondays.",
+    name: "One Piece OP24 - PAINT by I Don't Like Mondays.",
     videoId : "https://naedist.animemusicquiz.com/q29ldp.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -241,7 +241,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Saikou Toutatsu-ten by SEKAI NO OWARI",
+    name: "One Piece OP25 - Saikou Toutatsu-ten by SEKAI NO OWARI",
     videoId : "https://naedist.animemusicquiz.com/7ub4ci.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
@@ -251,7 +251,7 @@ export const songs = [
     }
 },
 	{
-    name: "One Piece - Uuuuus! by Hiroshi Kitadani",
+    name: "One Piece OP26 - Uuuuus! by Hiroshi Kitadani",
     videoId : "https://naedist.animemusicquiz.com/fsfc2w.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
