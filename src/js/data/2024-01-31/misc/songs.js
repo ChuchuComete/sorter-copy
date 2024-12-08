cb5e425ea917ac3bb04af3adfcaf8544df7cdd08
ChuchuComete <160599605+ChuchuComete@users.noreply.github.com>
2024-12-08 14:08:21 +0100

    Update songs.js

--- src/js/data/2024-01-31/misc/songs.js
+++ src/js/data/2024-01-31/misc/songs.js
@@ -1,8 +1,8 @@
 
 export const songs = [
 	{
-    name: "1.0. Yu-Gi-Oh! VRAINS - glory by BAND-MAID",
-    videoId : "https://naedist.animemusicquiz.com/01kveo.webm",
+    name: "1.0. Major - Kokoroe by ROAD OF MAJOR",
+    videoId : "https://naedist.animemusicquiz.com/7ritrj.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -11,8 +11,8 @@ export const songs = [
     }
 },
 	{
-    name: "2.0. Log Horizon: Entaku Houkai - Different by BAND-MAID",
-    videoId : "https://naedist.animemusicquiz.com/39bvmp.webm",
+    name: "2.0. Major - Saraba Aoki Omokage by ROAD OF MAJOR",
+    videoId : "https://naedist.animemusicquiz.com/g2dr6a.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -21,8 +21,8 @@ export const songs = [
     }
 },
 	{
-    name: "3.0. Quiz Tokiko-san - Carry on living by BAND-MAID",
-    videoId : "https://www.youtube.com/watch?v=Rylf8-NaJuk",
+    name: "3.0. Major - PLAY THE GAME by ROAD OF MAJOR",
+    videoId : "https://naedist.animemusicquiz.com/m02fdw.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -31,8 +31,8 @@ export const songs = [
     }
 },
 	{
-    name: "4.0. Platinum End - Sense by BAND-MAID",
-    videoId : "https://naedist.animemusicquiz.com/u2qey0.webm",
+    name: "4.0. Major - RISE by Kohei Otomo",
+    videoId : "https://naedist.animemusicquiz.com/zijh3n.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -41,8 +41,248 @@ export const songs = [
     }
 },
 	{
-    name: "5.0. Kengan Ashura - Shambles by BAND-MAID",
-    videoId : "https://naedist.animemusicquiz.com/zbxu2g.webm",
+    name: "5.0. Major - Hey! Hey! Alright by Scha Dara Parr+Kaela Kimura",
+    videoId : "https://naedist.animemusicquiz.com/9bbkb2.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "6.0. Major - Kokoroe by TRIPLANE",
+    videoId : "https://naedist.animemusicquiz.com/t5x03q.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "7.0. Major - Step by Beni Arashiro",
+    videoId : "https://naedist.animemusicquiz.com/930dwb.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "8.0. Major - Faraway by PARADISE GO!! GO!!",
+    videoId : "https://naedist.animemusicquiz.com/43ba03.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "9.0. Major - WONDERLAND by MAY",
+    videoId : "https://naedist.animemusicquiz.com/1vkyay.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "10.0. Major - Shoboi Kao Sunna yo Baby by The Loose Dogs",
+    videoId : "https://naedist.animemusicquiz.com/m1n8gd.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "11.0. Major - Strike Party!!! by BeForU",
+    videoId : "https://naedist.animemusicquiz.com/kc1g6f.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "12.0. Major - Yoru ni Nareba by The Loose Dogs",
+    videoId : "https://naedist.animemusicquiz.com/raw85d.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "13.0. Major - ONE DAY by The Loose Dogs",
+    videoId : "https://naedist.animemusicquiz.com/m80ouq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "14.0. Major - Ame Nochi Niji-iro by Showtaro Morikubo×The Loose Dogs feat. Maki Ohguro",
+    videoId : "https://naedist.animemusicquiz.com/euyrpq.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "15.0. Major - Stay with me by Hitomi Shimatani",
+    videoId : "https://naedist.animemusicquiz.com/wfynr6.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "16.0. Major - Jibun Color by Yu Nakamura",
+    videoId : "https://naedist.animemusicquiz.com/vq017m.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "17.0. Major - Twilight Star by Megamasso",
+    videoId : "https://naedist.animemusicquiz.com/riybuh.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "18.0. Major - Zutto Mae kara by French Kiss",
+    videoId : "https://naedist.animemusicquiz.com/h13qit.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "19.0. Major: Yuujou no Winning Shot - Tsubasa by Remioromen",
+    videoId : "https://naedist.animemusicquiz.com/1cdpxr.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "20.0. Major: Message - Kokoroe by Showtaro Morikubo",
+    videoId : "https://naedist.animemusicquiz.com/fuzt6a.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "21.0. Major: World Series-hen - Yume no Shunkan e - JUST DO IT by CLUTCHO",
+    videoId : "https://naedist.animemusicquiz.com/jxr4bl.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "22.0. Major: World Series-hen - Yume no Shunkan e - Time capsule by CLUTCHO",
+    videoId : "https://naedist.animemusicquiz.com/wocsns.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "23.0. Major 2nd - Koete Ike by Kyuuso Nekokami",
+    videoId : "https://naedist.animemusicquiz.com/241xwi.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "24.0. Major 2nd - Dream Catcher by Berry Goodman",
+    videoId : "https://naedist.animemusicquiz.com/2a75ft.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "25.0. Major 2nd - Pride by Yuu Takahashi",
+    videoId : "https://naedist.animemusicquiz.com/vp908v.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "26.0. Major 2nd - SAIREN by Reol",
+    videoId : "https://naedist.animemusicquiz.com/0z2usy.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "27.0. Major 2nd - Answer by Leo Ieiri",
+    videoId : "https://naedist.animemusicquiz.com/m90ypk.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "28.0. Major 2nd - Shiroi Doro by Mone Kamishiraishi",
+    videoId : "https://naedist.animemusicquiz.com/cj2fvx.webm",
+    videoSource: VIDEOSOURCE.CATBOX,
+    opts:{
+        singer:[
+        "OP"
+        ]
+    }
+},
+	{
+    name: "29.0. Major 2nd - One by SHE'S",
+    videoId : "https://naedist.animemusicquiz.com/v5rixm.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
@@ -50,8 +290,8 @@ export const songs = [
         ]
     }
 },{
-    name: "6.0. Grendizer U - Protect You by BAND-MAID",
-    videoId : "https://naedist.animemusicquiz.com/2gv5tc.webm",
+    name: "30.0. Major 2nd - IDENTITY by Ame no Parade",
+    videoId : "https://naedist.animemusicquiz.com/bg7ntd.webm",
     videoSource: VIDEOSOURCE.CATBOX,
     opts:{
         singer:[
